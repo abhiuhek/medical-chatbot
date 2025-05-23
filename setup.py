31fafbaa51650adf41a088e7cfc4cba007d9ebda
Abhishek <a27578900@gmail.com>
2025-05-23 23:47:50 +0530

    app.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='medical_chatbot',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Abhishek',
+    author_email='av973517@gmail.com',
+    description='A medical chatbot built using LangChain and Flask',
+)
